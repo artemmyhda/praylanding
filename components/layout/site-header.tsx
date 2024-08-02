a99a9285b396ed2fa33b65953892ee80f4f2dcc0
Artem Myhda <artem.myhda@outlook.com>
2024-08-02 17:02:02 +0200

    build: fix header shadow

--- components/layout/site-header.tsx
+++ components/layout/site-header.tsx
@@ -1,6 +1,5 @@
 "use client"
 
-import useScroll from "@/lib/hooks/use-scroll"
 import { cn } from "@/lib/utils"
 import { MainNav } from "@/components/layout/main-nav"
 import { MobileNav } from "@/components/layout/mobile-nav"
@@ -9,17 +8,15 @@ import { siteConfig } from "@/config/site";
 import { FaTelegram, FaTwitter } from "react-icons/fa";
 
 export function SiteHeader() {
-  const scrolled = useScroll(0)
 
   return (
       <header
           className={cn(
-              "sticky top-0 z-50 w-full border-0 backdrop-blur transition-all",
-              scrolled && "bg-background/50"
+              "sticky top-0 z-50 w-full backdrop-blur transition-all border-0 border-stone-300 shadow-lg",
           )}
       >
         <div className="flex h-20 items-center">
-          <div className="flex w-full items-center justify-center hidden md:flex">
+          <div className="w-full items-center justify-center hidden md:flex">
             <MainNav />
           </div>
           <MobileNav />
